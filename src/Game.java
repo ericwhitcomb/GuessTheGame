fe4b9f933ace9ecbcf1a29f811617c617e9acaf8
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-01-09 18:52:57 -0700

    Create game class and object with simple print method

--- src/Game.java
+++ src/Game.java
@@ -4,24 +4,67 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
+
+    /**
+     * Fields
+     */
+    private String movie;
+    private int count;
+
+    Game() {
+        this.movie = null;
+    }
+
+    Game(String movie) {
+        this.setMovie(movie);
+    }
+
+    /**
+     * Get movie
+     * @return
+     */
+    public String getMovie() {
+        return movie;
+    }
+
+    /**
+     * Set movie
+     * @param movie
+     */
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+    public void print() {
+        System.out.println("You are guessing:" + this.getMovie());
+        System.out.println("You have guessed (" + this.count + ") wrong letters:");
+        System.out.print("Guess a letter:");
+    }
+
+    /**
+     * Main method, runs game
+     *
+     * @param args
+     * @throws FileNotFoundException
+     */
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("movies.txt");
         Scanner scanner = new Scanner(file);
 
         ArrayList<String> movies = new ArrayList<String>();
-        System.out.println(movies.size());
+//        System.out.println(movies.size());
 
         while (scanner.hasNextLine()) {
             movies.add(scanner.nextLine().trim());
         }
-        System.out.println(movies.size());
+//        System.out.println(movies.size());
 
         int n = (int) (Math.random() * movies.size() - 1);
-        System.out.println(n);
+//        System.out.println(n);
 
-        System.out.println(movies.get(n));
+//        System.out.println(movies.get(n));
 
-        
-
+        Game game = new Game(movies.get(n));
+        game.print();
     }
 }
