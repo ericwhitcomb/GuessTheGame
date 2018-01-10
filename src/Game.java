ae80218c8a1982912623f5f7664fe133292fd752
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-01-09 18:21:38 -0700

    Reading movies into arraylist

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("movies.txt");
         Scanner scanner = new Scanner(file);
-        System.out.println(scanner.nextLine());
+
+        ArrayList<String> movies = new ArrayList<String>();
+        System.out.println(movies.size());
+
+        while (scanner.hasNextLine()) {
+            movies.add(scanner.nextLine().trim());
+        }
+        System.out.println(movies.size());
+
+        int n = (int) (Math.random() * movies.size() - 1);
+        System.out.println(n);
+
+        System.out.println(movies.get(n));
+
+        
+
     }
 }
