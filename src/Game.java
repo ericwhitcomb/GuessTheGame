ff0a590d66e129d27d5b29abf80aeefbd1daf61b
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-01-09 17:41:40 -0700

    Reading from file

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Game {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("movies.txt");
+        Scanner scanner = new Scanner(file);
+        System.out.println(scanner.nextLine());
     }
 }
